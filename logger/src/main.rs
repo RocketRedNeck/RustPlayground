a7d821821646b1db10f6d1b26cabc25bb3ae5f86
Rocky Red <the.rocketredneck@gmail.com>
2023-07-29 12:44:10 -0700

    make max threads adjustable

--- logger/src/main.rs
+++ logger/src/main.rs
@@ -2,21 +2,21 @@ use std::io;
 use std::sync::mpsc::{self, Receiver};
 use std::thread;
 
-fn start_senders()
+fn start_senders(max_senders: u32)
     -> (mpsc::Receiver<String>, Vec<thread::JoinHandle<()>>)
 {
     let (sender, receiver) = mpsc::sync_channel(10000);
 
     let mut handles: Vec<thread::JoinHandle<()>> = Vec::new();
-    for j in 0..3
+    for j in 0u32..max_senders
     {
         let my_sender = sender.clone();
         let handle: thread::JoinHandle<()> = thread::spawn(move || {
-            let mut i = 0i32;
+            let mut i = 0u32;
             loop {
                 let s : String = String::new() + "Thread_" + &j.to_string() + "_";
                 if my_sender.send(s + &i.to_string()).is_err() {
-                    println!("Thread_{} detected receiver stop",j);
+                    println!("Thread_{} detected receiver stop on i = {}",j,i);
                     break;
                 }
                 i += 1;
@@ -44,7 +44,7 @@ fn start_logger(messages : Receiver<String> )
     }
 }
 fn main() {
-    let (messages, handles) = start_senders();
+    let (messages, handles) = start_senders(20u32);
     start_logger(messages);
 
     for handle in handles
