48c5fdc3fee550e9c3ee9e48181d44a18288749d
Rocky Red <the.rocketredneck@gmail.com>
2023-07-29 13:35:05 -0700

    2000 threads with 100000 depth

--- logger/src/main.rs
+++ logger/src/main.rs
@@ -1,11 +1,12 @@
 use std::io;
 use std::sync::mpsc::{self, Receiver};
 use std::thread;
+use std::time::{self, Instant};
 
-fn start_senders(max_senders: u32)
+fn start_senders(max_senders: u32, depth: usize)
     -> (mpsc::Receiver<String>, Vec<thread::JoinHandle<()>>)
 {
-    let (sender, receiver) = mpsc::sync_channel(10000);
+    let (sender, receiver) = mpsc::sync_channel(depth);
 
     let mut handles: Vec<thread::JoinHandle<()>> = Vec::new();
     for j in 0u32..max_senders
@@ -13,11 +14,25 @@ fn start_senders(max_senders: u32)
         let my_sender = sender.clone();
         let handle: thread::JoinHandle<()> = thread::spawn(move || {
             let mut i = 0u32;
+            let mut min_elapsed = u128::MAX;
+            let mut max_elapsed = 0u128;
+            let mut total_elapsed = 0u128;
             loop {
                 let s : String = String::new() + "Thread_" + &j.to_string() + "_";
+                let now = Instant::now();
                 if my_sender.send(s + &i.to_string()).is_err() {
-                    println!("Thread_{} detected receiver stop on i = {}",j,i);
+                    println!("Thread_{} detected receiver stop on i = {} with elapsed min/max/total = {} / {} / {} micros ",j,i,min_elapsed,max_elapsed, total_elapsed);
                     break;
+                }
+                let elapsed = now.elapsed().as_micros();
+                total_elapsed += elapsed;
+                if elapsed > max_elapsed
+                {
+                    max_elapsed = elapsed;
+                }
+                if elapsed < min_elapsed
+                {
+                    min_elapsed = elapsed;
                 }
                 i += 1;
             }
@@ -44,7 +59,7 @@ fn start_logger(messages : Receiver<String> )
     }
 }
 fn main() {
-    let (messages, handles) = start_senders(20u32);
+    let (messages, handles) = start_senders(2000, 100000);
     start_logger(messages);
 
     for handle in handles
